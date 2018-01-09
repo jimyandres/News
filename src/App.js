ebe085fefb2e4008f4af6980d7e20f341e02a65f
jimyandres <jimyandres@utp.edu.co>
2018-01-09 15:52:06 -0500

    SVG Loading icon added

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
       error: null,
+      isLoading: false,
     }
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -58,11 +59,13 @@ class App extends Component {
       results: {
         ...results,
         [searchKey]: {hits: updatedHits, page}
-      }
+      },
+      isLoading: false,
     });
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
+    this.setState({isLoading: true});
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
@@ -110,6 +113,7 @@ class App extends Component {
       results,
       searchKey,
       error,
+      isLoading,
     } = this.state;
 
     const page = (
@@ -152,9 +156,13 @@ class App extends Component {
             />
           }
           <div className="interactions">
-            <Button onClick={() => this.fetchSearchTopStories(searchKey, page+1)}>
-              More
-            </Button>
+            {
+              isLoading
+              ? <Loading />
+              : <Button onClick={() => this.fetchSearchTopStories(searchKey, page+1)}>
+                More
+              </Button>
+            }
           </div>
         </div>
       </div>
@@ -162,17 +170,34 @@ class App extends Component {
   }
 }
 
-const Search = ({value, onChange, onSubmit, children}) =>
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>
+class Search extends Component {
+  componentDidMount() {
+    if(this.input) {this.input.focus();}
+  }
+
+  render() {
+    const {
+      value,
+      onChange,
+      onSubmit,
+      children
+    } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={(node) => { this.input = node; }}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    );
+  }
+}
 
 Search.propTypes = {
   value: PropTypes.string,
@@ -226,6 +251,25 @@ Button.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
 };
+
+const Loading = () =>
+  <div class="loader loader--style1" title="0">
+    <svg id="loader-1" width="40px" height="40px">
+      <path opacity="0.2" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
+        s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
+        c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
+      <path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
+        C22.32,8.481,24.301,9.057,26.013,10.047z">
+        <animateTransform attributeType="xml"
+          attributeName="transform"
+          type="rotate"
+          from="0 20 20"
+          to="360 20 20"
+          dur="0.5s"
+          repeatCount="indefinite"/>
+      </path>
+    </svg>
+  </div>
 
 const largeColumn = {
   width: '40%',
