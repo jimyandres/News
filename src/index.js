29dc710135af0d90756851e4ced9cf821246e3c8
jimyandres <jimyandres@utp.edu.co>
2018-05-06 14:23:50 -0500

    Primary color updated

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import orange from 'material-ui/colors/orange';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[800],
+      dark: orange[700]
+    },
+  },
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root'));
 registerServiceWorker();
 
 if (module.hot) {
