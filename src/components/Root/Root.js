bff5ec2206bf8c1448d2db553d2baba3dfb3d638
jimyandres <jimyandres@utp.edu.co>
2018-05-12 12:45:09 -0500

    Refactory: Root component added to separate the App component with the Material-ui theme applied

---
+++ src/components/Root/Root.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import App from '../App';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import orange from 'material-ui/colors/orange';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[800],
+      dark: orange[700]
+    },
+  },
+});
+
+const Root = () =>
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
-import orange from 'material-ui/colors/orange';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: orange[800],
-      dark: orange[700]
-    },
-  },
-});
+import Root from './components/Root';
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-    <App />
-  </MuiThemeProvider>,
+  <Root />,
   document.getElementById('root'));
 registerServiceWorker();
 
