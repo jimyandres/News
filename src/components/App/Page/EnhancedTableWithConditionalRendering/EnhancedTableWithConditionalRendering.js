1d978a73d2df9d75d7bb775fdcd66abebdd16cfc
jimyandres <jimyandres@utp.edu.co>
2018-05-12 13:44:43 -0500

    Refactory: Table Component moved into its father folder

--- src/components/App/Page/EnhancedTableWithConditionalRendering/EnhancedTableWithConditionalRendering.js
+++ src/components/App/Page/EnhancedTableWithConditionalRendering/EnhancedTableWithConditionalRendering.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { compose } from 'recompose';
 
-import Table from '../../../Table';
+import Table from './Table';
 import Button from '../../../Button';
 
 const withInfiniteScroll = (conditionalFn) => (Component) =>
--- src/components/App/Page/EnhancedTableWithConditionalRendering/Table/Table.js
+++ src/components/App/Page/EnhancedTableWithConditionalRendering/Table/Table.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button from '../Button';
+import Button from '../../../../Button';
 import { sortBy } from 'lodash';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
