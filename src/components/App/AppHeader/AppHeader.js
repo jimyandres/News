06584561f10d2821351e2ffaa9e17f9cf55b1b54
jimyandres <jimyandres@utp.edu.co>
2018-05-12 13:10:30 -0500

    Refactory: Component AppHeader created

---
+++ src/components/App/AppHeader/AppHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../../logo.svg';
+
+const AppHeader = () =>
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">Welcome to Hacker News with React</h1>
+  </header>
+
+export default AppHeader;
