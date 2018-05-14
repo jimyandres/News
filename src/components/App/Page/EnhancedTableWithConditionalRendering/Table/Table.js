f1a67fbd510362b82ecff6f82c63f7d89dc23def
jimyandres <jimyandres@utp.edu.co>
2018-05-14 18:45:58 -0500

    Table snapshot and tests updated, and some minor changes

--- src/components/App/Page/EnhancedTableWithConditionalRendering/Table/Table.js
+++ src/components/App/Page/EnhancedTableWithConditionalRendering/Table/Table.js
@@ -3,12 +3,12 @@ import Button from '../Button';
 import { sortBy } from 'lodash';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import './index.css';
+import './Table.css';
 import { withStyles } from 'material-ui/styles';
 import ExpansionPanel, {
-  ExpansionPanelDetails,
-  ExpansionPanelSummary,
-  ExpansionPanelActions,
+	ExpansionPanelDetails,
+	ExpansionPanelSummary,
+	ExpansionPanelActions,
 } from 'material-ui/ExpansionPanel';
 import Typography from 'material-ui/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -16,186 +16,188 @@ import Chip from 'material-ui/Chip';
 import Divider from 'material-ui/Divider';
 
 const SORTS = {
-  NONE: list => list,
-  TITLE: list => sortBy(list, 'title'),
-  AUTHOR: list => sortBy(list, 'author'),
-  COMMENTS: list => sortBy(list, 'num_comments').reverse(),
-  POINTS: list => sortBy(list, 'points').reverse(),
+	NONE: list => list,
+	TITLE: list => sortBy(list, 'title'),
+	AUTHOR: list => sortBy(list, 'author'),
+	COMMENTS: list => sortBy(list, 'num_comments').reverse(),
+	POINTS: list => sortBy(list, 'points').reverse(),
 };
 
 const styles = theme => ({
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    flexBasis: '66.66%',
-    // whiteSpace: 'nowrap',
-    // overflow: 'hidden',
-    // textOverflow: 'ellipsis',
-  },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary,
-  },
-  icon: {
-    verticalAlign: 'bottom',
-    height: 20,
-    width: 20,
-  },
-  details: {
-    alignItems: 'center',
-  },
-  column2: {
-    flexBasis: '66.66%',
-  },
-  column: {
-    flexBasis: '33.33%',
-  },
-  helper: {
-    borderLeft: `2px solid ${theme.palette.divider}`,
-    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 3}px`,
-  },
-  link: {
-    color: theme.palette.primary.main,
-    textDecoration: 'none',
-    '&:hover': {
-      textDecoration: 'underline',
-    },
-  },
+	heading: {
+		fontSize: theme.typography.pxToRem(15),
+		flexBasis: '66.66%',
+		// whiteSpace: 'nowrap',
+		// overflow: 'hidden',
+		// textOverflow: 'ellipsis',
+	},
+	secondaryHeading: {
+		fontSize: theme.typography.pxToRem(15),
+		color: theme.palette.text.secondary,
+	},
+	icon: {
+		verticalAlign: 'bottom',
+		height: 20,
+		width: 20,
+	},
+	details: {
+		alignItems: 'center',
+	},
+	column2: {
+		flexBasis: '66.66%',
+	},
+	column: {
+		flexBasis: '33.33%',
+	},
+	helper: {
+		borderLeft: `2px solid ${theme.palette.divider}`,
+		padding: `${theme.spacing.unit}px ${theme.spacing.unit * 3}px`,
+	},
+	link: {
+		color: theme.palette.primary.main,
+		textDecoration: 'none',
+		'&:hover': {
+			textDecoration: 'underline',
+		},
+	},
 });
 
 class Table extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      sortKey: 'NONE',
-      isSortReverse: false,
-      expanded: null,
-    };
+		this.state = {
+			sortKey: 'NONE',
+			isSortReverse: false,
+			expanded: null,
+		};
 
-    this.onSort = this.onSort.bind(this);
-  }
+		this.onSort = this.onSort.bind(this);
+	}
 
-  handleChange = panel => (event, expanded) => {
-    this.setState({
-      expanded: expanded ? panel : false,
-    });
-  };
+	handleChange(panel) {
+		return (event, expanded) => {
+    	this.setState({
+      	expanded: expanded ? panel : false,
+    	});
+  	};
+	}
 
-  onSort(sortKey) {
-    const isSortReverse = this.state.sortKey === sortKey && !this.state.isSortReverse;
-    this.setState({sortKey, isSortReverse});
-  }
+	onSort(sortKey) {
+		const isSortReverse = this.state.sortKey === sortKey && !this.state.isSortReverse;
+		this.setState({sortKey, isSortReverse});
+	}
 
-  render () {
-    const {
-      list,
-      onDismiss,
-      classes
-    } = this.props;
+	render () {
+		const {
+			list,
+			onDismiss,
+			classes
+		} = this.props;
 
-    const {
-      sortKey,
-      isSortReverse,
-      expanded,
-    } = this.state;
+		const {
+			sortKey,
+			isSortReverse,
+			expanded,
+		} = this.state;
 
-    const sortedList = SORTS[sortKey](list);
-    const reverseSortedList = isSortReverse
-      ? sortedList.reverse()
-      : sortedList;
+		const sortedList = SORTS[sortKey](list);
+		const reverseSortedList = isSortReverse
+			? sortedList.reverse()
+			: sortedList;
 
-    return (
-      <div className="table">
-        <div className="table-header">
-          <SortList
-            sortKeys={[
-              {name: 'Title'},
-              {name: 'Author'},
-              {name: 'Comments'},
-              {name: 'Points'},
-            ]}
-            onSort={this.onSort}
-            activeSortKey={sortKey}
-            isSortReverse={isSortReverse}
-          />
-        </div>
-        { reverseSortedList.map(item =>
-          <ExpansionPanel
-            key={item.objectID}
-            expanded={expanded === item.objectID}
-            onChange={this.handleChange(item.objectID)}
-          >
-            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon  />}>
-              <Typography className={classes.heading}>{ item.title }</Typography>
-              <Typography className={classes.secondaryHeading}>by {item.author}</Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <div className={classes.column2}>
-                {item._tags.map((tag, key) =>
-                  <Chip key={key} label={tag} className={classes.chip} />
-                )}
-              </div>
-              <div className={classNames(classes.column, classes.helper)}>
-                <Typography variant="caption">
-                  <b>{ item.title }</b><br /><br />
-                  <b>Points:</b> {item.points}<br />
-                  <b>Comments:</b> {item.num_comments}<br /><br />
-                  <a href={item.url} className={classes.link}>
+		return (
+			<div className="table">
+				<div className="table-header">
+					<SortList
+						sortKeys={[
+							{name: 'Title'},
+							{name: 'Author'},
+							{name: 'Comments'},
+							{name: 'Points'},
+						]}
+						onSort={this.onSort}
+						activeSortKey={sortKey}
+						isSortReverse={isSortReverse}
+					/>
+				</div>
+				{ reverseSortedList.map(item =>
+					<ExpansionPanel
+						key={item.objectID}
+						expanded={expanded === item.objectID}
+						onChange={this.handleChange(item.objectID)}
+					>
+						<ExpansionPanelSummary expandIcon={<ExpandMoreIcon  />}>
+							<Typography className={classes.heading}>{ item.title }</Typography>
+							<Typography className={classes.secondaryHeading}>by {item.author}</Typography>
+						</ExpansionPanelSummary>
+						<ExpansionPanelDetails>
+							<div className={classes.column2}>
+								{item._tags.map((tag, key) =>
+									<Chip key={key} label={tag} className={classes.chip} />
+								)}
+							</div>
+							<div className={classNames(classes.column, classes.helper)}>
+								<Typography variant="caption">
+									<b>{ item.title }</b><br /><br />
+									<b>Points:</b> {item.points}<br />
+									<b>Comments:</b> {item.num_comments}<br /><br />
+									<a href={item.url} className={classes.link}>
                     See more
-                  </a>
-                </Typography>
-              </div>
-            </ExpansionPanelDetails>
-            <Divider />
-            <ExpansionPanelActions>
-              <Button size="small" color="primary" onClick={() => onDismiss(item.objectID)}>
+									</a>
+								</Typography>
+							</div>
+						</ExpansionPanelDetails>
+						<Divider />
+						<ExpansionPanelActions>
+							<Button size="small" color="primary" onClick={() => onDismiss(item.objectID)}>
                 Dismiss
-              </Button>
-            </ExpansionPanelActions>
-          </ExpansionPanel>
-        )}
-      </div>
-    );
-  }
+							</Button>
+						</ExpansionPanelActions>
+					</ExpansionPanel>
+				)}
+			</div>
+		);
+	}
 }
 
 Table.propTypes = {
-  list: PropTypes.array,
-  sortKey: PropTypes.string,
-  onSort: PropTypes.func,
-  onDismiss: PropTypes.func,
+	list: PropTypes.array,
+	sortKey: PropTypes.string,
+	onSort: PropTypes.func,
+	onDismiss: PropTypes.func,
 };
 
 const SortList = ({sortKeys, ...rest}) => {
-  return sortKeys.map(key =>
-    <Sort
-      key={key.name}
-      sortKey={key.name.toUpperCase()}
-      {... rest}
-    >
-      {key.name}
-    </Sort>
-  );
-}
+	return sortKeys.map(key =>
+		<Sort
+			key={key.name}
+			sortKey={key.name.toUpperCase()}
+			{... rest}
+		>
+			{key.name}
+		</Sort>
+	);
+};
 
 const Sort = ({sortKey, onSort, activeSortKey, isSortReverse, children}) => {
-  const sortClass = classNames(
-    {'filter-active': sortKey === activeSortKey}
-  );
-  const arrowSort = classNames(
-    {'arrow arrow-up': sortKey === activeSortKey && !isSortReverse},
-    {'arrow arrow-down': sortKey === activeSortKey && isSortReverse}
-  );
-  return (
-    <Button
-      onClick={() => onSort(sortKey)}
-      style={{minWidth: '12em'}}
-      active={sortClass}
-    >
-      {children}
-      <div className='arrowContainer'><div className={arrowSort} /></div>
-    </Button>
-  );
-}
+	const sortClass = classNames(
+		{'filter-active': sortKey === activeSortKey}
+	);
+	const arrowSort = classNames(
+		{'arrow arrow-up': sortKey === activeSortKey && !isSortReverse},
+		{'arrow arrow-down': sortKey === activeSortKey && isSortReverse}
+	);
+	return (
+		<Button
+			onClick={() => onSort(sortKey)}
+			style={{minWidth: '12em'}}
+			active={sortClass}
+		>
+			{children}
+			<div className='arrowContainer'><div className={arrowSort} /></div>
+		</Button>
+	);
+};
 
 export default withStyles(styles)(Table);
---
+++ src/components/App/Page/EnhancedTableWithConditionalRendering/Table/Table.test.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { configure, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallowToJson } from 'enzyme-to-json';
+import Table from './index';
+
+configure({ adapter: new Adapter() });
+
+describe('Table', () => {
+
+	const props = {
+		list: [
+		  {
+		    "title": "Things to learn in React before using Redux",
+		    "url": "https://www.robinwieruch.de/learn-react-before-using-redux/",
+		    "author": "callumlocke",
+		    "points": 370,
+		    "num_comments": 104,
+		    "_tags": [
+		      "story",
+		      "author_callumlocke",
+		      "story_14811577"
+		    ],
+		    "objectID": "14811577",
+		  },
+		  {
+		    "title": "Show HN: ORY Editor – A rich editor for the browser, built with React and Redux",
+		    "url": "https://github.com/ory/editor?branch=master",
+		    "author": "jswizzard",
+		    "points": 369,
+		    "num_comments": 98,
+		    "_tags": [
+		      "story",
+		      "author_jswizzard",
+		      "story_14417340",
+		      "show_hn"
+		    ],
+		    "objectID": "14417340",
+		  },
+		  {
+		    "title": "A SoundCloud client in React and Redux",
+		    "url": "http://www.robinwieruch.de/the-soundcloud-client-in-react-redux/",
+		    "author": "rwieruch",
+		    "points": 253,
+		    "num_comments": 69,
+		    "_tags": [
+		      "story",
+		      "author_rwieruch",
+		      "story_11890229"
+		    ],
+		    "objectID": "11890229",
+		  },
+		  {
+		    "title": "Systemd redux: The end of Linux",
+		    "url": "http://blog.lusis.org/blog/2014/11/20/systemd-redux/",
+		    "author": "bcantrill",
+		    "points": 235,
+		    "num_comments": 463,
+		    "_tags": [
+		      "story",
+		      "author_bcantrill",
+		      "story_8639317"
+		    ],
+		    "objectID": "8639317",
+		  }
+		]
+	};
+
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<Table { ...props } />, div);
+	});
+
+	test('has a valid snapshot', () => {
+		const component = render(<Table { ...props } />);
+		expect(shallowToJson(component)).toMatchSnapshot();
+	});
+
+	it('shows two items in list', () => {
+		const element = mount(<Table { ...props } />);
+		expect(element.find('ExpansionPanel').length).toBe(4);
+	});
+
+});
